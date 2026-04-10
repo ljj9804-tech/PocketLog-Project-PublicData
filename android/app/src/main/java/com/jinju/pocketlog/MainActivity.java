389d76d273e16e740b594d493ab52cc653e286dd
leejinju <ljj9804@gmail.com>
2026-04-10 17:26:00 +0900

    first commit

---
+++ android/app/src/main/java/com/jinju/pocketlog/MainActivity.java
@@ -0,0 +1,6 @@
+package com.jinju.pocketlog;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/models/transaction.dart
@@ -0,0 +1,30 @@
+class Transaction {
+  final String id;      //고유번호
+  final String title;   //내역 제목
+  final int amount;     //금액
+  final DateTime date;  //날짜
+  final bool isIncome;  //true: 수입, false: 지출
+
+  Transaction({
+    required this.id,
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.isIncome,
+  });
+
+  factory Transaction.fromJson(Map<String, dynamic> json) {
+    return Transaction(
+      id: json['id'],
+      title: json['title'],
+      amount: json['amount'],
+      date: DateTime.parse(json['date']),
+      isIncome: json['isIncome'],
+    );
+  }
+
+
+}
+
+
+//test
